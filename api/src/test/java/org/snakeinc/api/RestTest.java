97107836edda22168f7c251f1a0371cc7c462053
Gaspachoo <gaspard.lefort@outlook.fr>
2026-01-06 18:11:15 +0100

    [add] Update integration tests - using TestContainer

--- api/src/test/java/org/snakeinc/api/RestTest.java
+++ api/src/test/java/org/snakeinc/api/RestTest.java
@@ -1,6 +1,8 @@
 package org.snakeinc.api;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.snakeinc.api.entity.Player;
@@ -8,24 +10,47 @@ import org.snakeinc.api.entity.PlayerParams;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.equalTo;
 
-@ActiveProfiles("test")
+//@ActiveProfiles("test")
 @SpringBootTest(classes = ApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RestTest {
-
 
     @LocalServerPort
     private Integer port;
 
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:16-alpine"
+    );
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
     @BeforeEach
     void setup() {
         RestAssured.baseURI = "http://localhost";
-        io.restassured.RestAssured.port = port;
+        RestAssured.port = port;
         RestAssured.basePath = "/api/v1";
     }
 
