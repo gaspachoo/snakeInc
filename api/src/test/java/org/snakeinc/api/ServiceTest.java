40eed2b29db4cd3ebc7b8c12985738548f0d5f83
Gaspachoo <gaspard.lefort@outlook.fr>
2026-01-06 15:36:52 +0100

    [edit] update service tests

--- api/src/test/java/org/snakeinc/api/ServiceTest.java
+++ api/src/test/java/org/snakeinc/api/ServiceTest.java
@@ -3,11 +3,22 @@ package org.snakeinc.api;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.snakeinc.api.entity.Player;
+import org.snakeinc.api.repository.PlayerRepo;
 import org.snakeinc.api.service.PlayerService;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
 public class ServiceTest {
+
+    @Mock
+    private PlayerRepo repo;
+
     @InjectMocks
     PlayerService service;
 
@@ -20,14 +31,16 @@ public class ServiceTest {
     public void testAddPlayer(){
         Player player = new Player("nom", 20);
         service.addPlayer(player);
-        assert(service.getPlayers().get(player.getId()) ==  player);
+        verify(repo, times(1)).save(player);
     }
 
     @Test
     public void testGetPlayers(){
-        Player player = new Player("nom", 20);
-        service.addPlayer(player);
-        assert(service.getPlayer(player.getId()) == player);
+        Player expectedPlayer = new Player("nom", 20);
+        when(repo.findById(1)).thenReturn(Optional.of(expectedPlayer));
+        Optional<Player> result = service.getPlayer(1);
+        assertTrue(result.isPresent());
+        verify(repo, times(1)).findById(1);
     }
 
 }
