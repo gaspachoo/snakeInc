a023be97057e397bc7c8052828202b84c8cb4834
Gaspachoo <gaspard.lefort@outlook.fr>
2026-01-06 17:12:53 +0100

    [add] Update integration tests - using H2

--- api/src/test/java/org/snakeinc/api/RestTest.java
+++ api/src/test/java/org/snakeinc/api/RestTest.java
@@ -6,7 +6,9 @@ import org.snakeinc.api.entity.PlayerParams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest(classes = ApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RestTest {
 
