e4764573ed5f9688bbe3fe3d1a68b64a6f2d21a5
Gaspachoo <gaspard.lefort@outlook.fr>
2026-01-13 22:49:16 +0100

    [add] add NoArgsConstructor and playedAt field to ScoreParams

--- api/src/main/java/org/snakeinc/api/entity/ScoreParams.java
+++ api/src/main/java/org/snakeinc/api/entity/ScoreParams.java
@@ -4,9 +4,13 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.snakeinc.api.validation.ExistingPlayer;
 
+import java.time.LocalDateTime;
+
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ScoreParams {
 
@@ -20,6 +24,8 @@ public class ScoreParams {
     @Pattern(regexp = "^(boaConstrictor|anaconda|python)$",
             message = "snake must be boaConstrictor, anaconda, or python")
     private String snake;
+
+    private LocalDateTime playedAt;
 
     public ScoreParams(int playerId, String snake) {
         this.playerId = playerId;
